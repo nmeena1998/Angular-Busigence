9e831679a3a4f9d7e51769d8e700bdf5dcafe539
nmeena1998 <neetmeena457@gmail.com>
2019-09-26 00:47:27 +0530

    UI changes

--- src/app/analyzer/analyzer.component.ts
+++ src/app/analyzer/analyzer.component.ts
@@ -11,6 +11,21 @@ flag1:boolean=false;
 flag2:boolean=false;
 filePath:string;
 
+databaseArray= [{
+  database:"d1",
+  collection:["c1","c2","c3"]
+}, 
+{
+  database:"d2",
+  collection:["c4","c5","c6"]
+},
+{
+  database:"d3",
+  collection:["c7","c8","c9"]
+}
+
+]
+
 tabBoolean2:boolean=false;
 
 
